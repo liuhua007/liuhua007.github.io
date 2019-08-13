23dca4beb7fae9cccb841a9c771dcf49382c3258
hua.liu <liuhua007@gmail.com>
2019-08-13 14:36:05 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -7,26 +7,40 @@ window.onload = () => {
   }
 }
 
-var deferredPrompt;
+var installPromptEvent;
 
 window.addEventListener('beforeinstallprompt', function (e) {
   console.log("received beforeinstallprompt....");
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+  installPromptEvent = e;
 
   showAddToHomeScreen();
-
 });
 
 function showAddToHomeScreen() {
-
   //var a2hsBtn = document.querySelector(".ad2hs-prompt");
-
   //a2hsBtn.style.display = "block";
-
   //a2hsBtn.addEventListener("click", addToHomeScreen);
   console.log("show add to home screen");
-
 }
+
+document.querySelector('#btn-install').addEventListener('click', () => {
+  if( !installPromptEvent ) {
+    return;
+  }
+
+  installPromptEvent.prompt();
+  installPromptEvent.userChoice.then( choiceResult => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('accepted to add to desktop')
+    } else {
+      console.log('cancel adding to desktop')
+    }
+  })
+})
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('has already installed this app');
+});
