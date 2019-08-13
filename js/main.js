9f07df6431fdb21497b17a2ed4d719072655ff7e
hua.liu <liuhua007@gmail.com>
2019-08-13 13:25:13 +0900

    test

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  showAddToHomeScreen();
+
+});
+
+function showAddToHomeScreen() {
+
+  //var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+  //a2hsBtn.style.display = "block";
+
+  //a2hsBtn.addEventListener("click", addToHomeScreen);
+  console.log("show add to home screen");
+
+}
